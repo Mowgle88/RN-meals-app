87365412ef789cecc0be19d705b223b572c05fad
Mowgle88 <mowgle1988@gmail.com>
2022-08-28 01:37:35 +0300

    feat: use useSelector and icon change on meal-detail-screen

--- store/redux/store.ts
+++ store/redux/store.ts
@@ -1,7 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
+import favoritesReducer from './favorites'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favoritesMeal: favoritesReducer
+  },
 })
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
