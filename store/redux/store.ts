8710a89d16201cd796a798d906238adef89fade6
Mowgle88 <mowgle1988@gmail.com>
2022-08-27 23:38:56 +0300

    feat: add redux to project

---
+++ store/redux/store.ts
@@ -0,0 +1,7 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {},
+})
